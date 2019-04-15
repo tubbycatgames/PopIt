9e504a106ffd8e5cec8323f4164aaeaddad8e7d8
Gary DiCiano <garydiciano4@gmail.com>
2019-04-15 10:45:53 -0400

    Color Randomization
    The script will be able to choose from about 20 skin colors, 4 deeper reds for the zit itself, while the head gets random RGB values between 0.9 and 0.95

---
+++ Assets/Scripts/ColorRandomizer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : MonoBehaviour
+{
+    Renderer renderer;
+    public float r, g, b;
+
+    public float min, max;
+
+    public Vector3[] tones;
+
+    //Off Whites won't need a specific collection of colors
+    public bool usingOffWhites;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+        if (usingOffWhites)
+        {
+            r = Random.Range(min, max);
+            g = Random.Range(min, max);
+            b = Random.Range(min, max);
+        }
+        else
+        {
+            //Get a random index for one of the available lists of tones       
+            int i = Random.Range(0, tones.Length);
+
+            //Sets the RGB values to ones between 0 and 1
+            r = tones[i].x / 255f;
+            g = tones[i].y / 255f;
+            b = tones[i].z / 255f;
+        }
+
+        //For now, alpha will always be 1
+        Color newColor = new Color(r, g, b, 1);
+        renderer.material.SetColor("_Color", newColor);
+    }
+}
---
+++ Assets/Scripts/SkinColorRandomizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SkinColorRandomizer : MonoBehaviour
+{
+    //https://www.collectedwebs.com/art/colors/skin_tones/
+
+    Renderer renderer;
+    public float r, g, b;
+    public float a, aMin, aMax;
+
+    public Vector3[] skinTones;
+    public Vector3[] redTones;
+    public Vector3[] offWhiteTones;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        int i = Random.Range(0, skinTones.Length);
+        r = skinTones[i].x / 255f;
+        g = skinTones[i].y / 255f;
+        b = skinTones[i].z / 255f;
+        a = Random.Range(aMin, aMax);
+        Color newColor = new Color(r, g, b, a);
+        renderer.material.SetColor("_Color", newColor);
+    }
+}
